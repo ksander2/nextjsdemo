c5e3f7cfe0c6225c8ccf98856804ab6d900c426d
Соболев Александр Николаевич <a.sobolev@samolet.ru>
2025-02-08 23:05:41 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
 'use client'
 
 import { useEffect, useState } from "react";
-import { Divider, List, Typography } from 'antd';
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
+import { List } from 'antd';
 
 type User = {
   id: number;
